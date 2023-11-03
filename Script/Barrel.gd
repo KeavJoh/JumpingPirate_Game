9527937c39684c752736452509dada36a9c2f399
Kevin Johr <kevin.johr@posteo.de>
2023-11-03 23:46:28 +0100

    #2  add particel after destroy barrels

--- Script/Barrel.gd
+++ Script/Barrel.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+@export
+var hp = 5
+
+func checkDestroy():
+	hp -= 1
+	if(hp <= 0):
+		$AnimatedSprite2D.play("destroy")
+		$GPUParticles2D.emitting = true
+		await get_tree().create_timer(0.1).timeout
+		$AnimatedSprite2D.visible = false
+		await get_tree().create_timer(0.3).timeout
+		queue_free()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +25,5 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	if(area.is_in_group("FromPlayer")):
-		print("Player Hint!")
+		$AnimatedSprite2D.play("hit")
+		checkDestroy()
