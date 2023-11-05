457fb9f19c9c620b300896349acfd7abb41ef3f9
Kevin Johr <kevin.johr@posteo.de>
2023-11-05 22:51:53 +0100

    count diamonds and coins and add random drop after destroy barrel

--- Script/Barrel.gd
+++ Script/Barrel.gd
@@ -11,6 +11,7 @@ func checkDestroy():
 		await get_tree().create_timer(0.1).timeout
 		$AnimatedSprite2D.visible = false
 		await get_tree().create_timer(0.3).timeout
+		spawnRandomItemAfterDestroyBarrel(self.position)
 		queue_free()
 
 # Called when the node enters the scene tree for the first time.
@@ -22,6 +23,21 @@ func _ready():
 func _process(delta):
 	pass
 
+#Zufällige Spawnen von Coins oder Diamanten nach erfolgreicher zerstörung von Fässern. 
+func spawnRandomItemAfterDestroyBarrel(targetLocation):
+	var newItem = null
+	
+	randomize()
+	var randomItemSelection = randi_range(0,2)
+	match(randomItemSelection):
+		1: newItem = preload("res://Actors/GoldCoin.tscn")
+		2: newItem = preload("res://Actors/RedDiamond.tscn")
+		
+	if(newItem != null):
+		var newItemInstance = newItem.instantiate()
+		newItemInstance.position = targetLocation
+		get_parent().add_child(newItemInstance)
+	
 
 func _on_area_2d_area_entered(area):
 	if(area.is_in_group("FromPlayer")):
---
+++ Script/Global.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var redDiamonds = 0
+var goldCoins = 0
+
+func addRedDiamond():
+	redDiamonds += 1
+	
+func removeRedDiamond():
+	redDiamonds -= 1
+
+func addGoldCoin():
+	goldCoins += 1
+	
+func removeGoldCoin():
+	goldCoins -= 1
